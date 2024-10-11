81acf8de25613b7fe70c1a85db58ca9d5b4e4d90
JP-aloha <jaib@alohatechnology.com>
2024-10-11 16:55:54 +0530

    Udpated login view SwiftUI

--- Example-SwiftUI/Example-SwiftUI/Views/LoginView.swift
+++ Example-SwiftUI/Example-SwiftUI/Views/LoginView.swift
@@ -19,66 +19,80 @@ struct LoginView: View {
 	let userModel = UserViewModel()
 
     var body: some View {
-           ZStack {
-               Color.blue
-                   .edgesIgnoringSafeArea(.all)
-               VStack{
-                   HStack{
-                       Button("Cancel") {
-                           showLoginSheet = false
-                       }
-                       .foregroundColor(.white)
-                       .padding()
-                       Spacer()
-                   }
-                   Spacer()
-                   VStack(spacing: 20) {
-                       Text("Login")
-                           .font(.largeTitle)
-                       
-                       Picker("", selection: $selectedSegment) {
-                           ForEach(0..<segments.count) { index in
-                               Text(segments[index]).tag(index)
-                           }
-                       }
-                       .pickerStyle(SegmentedPickerStyle())
-                       
-                       TextField("Username", text: $username)
-                           .textFieldStyle(RoundedBorderTextFieldStyle())
-                           .disabled(isLoggedIn)
-                       
-                       SecureField("Password", text: $password)
-                           .textFieldStyle(RoundedBorderTextFieldStyle())
-                           .disabled(isLoggedIn)
-                       
-                       HStack {
-                           Button("Logout") {
-                               userModel.logOut()
-                               showLoginSheet = false
-                               BlueTriangle._setMetrics(nil, forKey: "user")
-                               BlueTriangle._setMetrics(false, forKey: "isPremium")
-                           }
-                           .disabled(!isLoggedIn)
-                           .padding()
-                           
-                           Button("Login"){
-                               
-                               let isPremioum = selectedSegment
-                               userModel.loggedIn(username, pass: password, isPremium: selectedSegment)
-                               BlueTriangle._setMetrics(username, forKey: "user")
-                               BlueTriangle._setMetrics((selectedSegment != 0) ? true : false, forKey: "isPremium")
-                               showLoginSheet = false
-                           }
-                           .disabled(isLoggedIn)
-                           .padding()
-                       }
-                   }
-                   .frame(width: 300)
-                   .padding()
-                   .background(Color.white)
-                   Spacer()
-               }
-           }
+		ZStack {
+			Color.blue
+				.edgesIgnoringSafeArea(.all)
+			VStack{
+				HStack{
+					Button("Cancel") {
+						showLoginSheet = false
+					}
+					.foregroundColor(.white)
+					.padding()
+					Spacer()
+				}
+				Spacer()
+				VStack(spacing: 20) {
+					
+					if (!isLoggedIn) {
+						Text("Login")
+							.font(.largeTitle)
+						
+						Picker("", selection: $selectedSegment) {
+							ForEach(0..<segments.count) { index in
+								Text(segments[index]).tag(index)
+							}
+						}
+						.pickerStyle(SegmentedPickerStyle())
+						
+						TextField("Username", text: $username)
+							.textFieldStyle(RoundedBorderTextFieldStyle())
+							.disabled(isLoggedIn)
+						
+						SecureField("Password", text: $password)
+							.textFieldStyle(RoundedBorderTextFieldStyle())
+							.disabled(isLoggedIn)
+					} else {
+						Text(username)
+							.font(.largeTitle)
+							.padding(.bottom, 10)
+						
+						Text(segments[selectedSegment])
+							.font(Font.system(size: 16, weight: .regular))//							.padding()
+						
+					}
+					
+					HStack {
+						if (isLoggedIn) {
+							Button("Logout") {
+								userModel.logOut()
+								BlueTriangle._setMetrics(nil, forKey: "user")
+								BlueTriangle._setMetrics(false, forKey: "isPremium")
+								username = ""
+								password = ""
+								isLoggedIn = false
+							}
+							.padding()
+						} else {
+							Button("Login"){
+								if (!username.isEmpty && !password.isEmpty) {
+									let isPremioum = selectedSegment
+									userModel.loggedIn(username, pass: password, isPremium: selectedSegment)
+									BlueTriangle._setMetrics(username, forKey: "user")
+									BlueTriangle._setMetrics((selectedSegment != 0) ? true : false, forKey: "isPremium")
+									isLoggedIn = true
+								}
+							}
+							.padding()
+						}
+					}
+				}
+				.frame(width: 300)
+				.padding()
+				.background(Color.white)
+				Spacer()
+			}
+			}
 		   .onAppear {
 			   if let user = userModel.loggedInUser() {
 				   username = user.name
